946d83eb8d35f50aee977db5000afa2ac20b3a4c
Saeed <mohammad-reza.eskandari@pinkswan.ch>
2023-10-23 13:58:13 +0330

    first release candidate test

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -7,6 +7,28 @@ import { PasswordValidator } from '../src'
 
 describe('Common render', () => {
   it('renders without crashing', () => {
-    render(<PasswordValidator />)
+    render(
+      <PasswordValidator
+        ruleSet={[
+          {
+            regex: '^.{8,32}$',
+            message: 'Must be 8 to 32 character',
+          },
+          {
+            regex: '(?=.*[a-z])',
+            message: 'At least one lower case letter exists',
+          },
+          {
+            regex: '(?=.*[A-Z])',
+            message: 'At least one upper case letter exists',
+          },
+          {
+            regex: '(?=.*\\d)',
+            message: 'At least one digit exists',
+          },
+        ]}
+        passwordValue=''
+      />,
+    )
   })
 })
